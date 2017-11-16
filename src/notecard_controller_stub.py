5dd1b753611ba45950f26d547f0dc18da1f64d30
brentongk <bkirsch3@jhu.edu>
2017-11-15 21:58:03 -0800

    The initial and stub classes for Notecards

---
+++ src/notecard_controller_stub.py
@@ -0,0 +1,14 @@
+from notecardcontroller import NoteCardController
+ 
+class NoteCardControllerStub:
+ 	"""Receives input from the User Interface subsystem and translates
+ 	 the request into the appropriate call to mark the Note Card instance,
+ 	  thus updating its internal state."""
+ 
+ 
+ 	 def __init__(self):
+ 
+ 	  	self._note_card = {}
+ 
+ 	 def mark(self, x: int, y: int):
+ 	  	notecard.mark(x, y) 
---
+++ src/notecard_stub.py
@@ -0,0 +1,16 @@
+from notecard import NoteCard
+ 
+class NoteCardStub:
+ 	"""Contains an array of lists. Each list is mapped to the associated color
+ 	 of an opponent and contains a symbol for each suspect, weapon, and place; 
+ 	 either an X, a ?, a ✓, or nothing. Has the ability to mark cards that were 
+ 	 shown (or deduced)	which only requires changing the symbol in the associated
+ 	 list."""
+ 
+ 
+ 	 def __init__(self):
+ 
+ 	 	self._note_state = [[]]
+ 
+ 	 def mark(self, x: int, y: int):
+ 	 	"""note_state[x][y] = (note_state[x][y] + 1) % NUMBER_OF_MARKS"""
