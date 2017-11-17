c291d619754bab0eb3bad4e094fcd7efb0204ee5
Sean <yo09975@umbc.edu>
2017-11-16 20:54:17 -0500

    Added Message and MessageType enumeration
    Stubbed the Message class and added a few possible values to the
    MessageType enum.

---
+++ src/network/message.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    MOVEMENT = 1
+    SUGGESTION = 2
+    SUGGESTION_RESP = 3
+    ACCUSATION = 4
+
+
+class Message:
+    """
+    Stores information to be transmitted between a client and server
+    """
+
+    """ Constructor """
+    def __init__(self, uuid, msg_type, payload):
+        """ Unique identifier for the originator of the Message """
+        self._uuid = uuid
+        """ msg_type must be in the MessageType enum """
+        if isinstance(msg_type, MessageType):
+            self._msg_type = msg_type
+        else:
+            raise ValueError
+        """ payload is a string containing the actual data """
+        self._payload = payload
+
+    """ Getter for uuid """
+    def get_uuid(self):
+        return self._uuid
+
+    """ Getter for msg_type """
+    def get_msg_type(self):
+        return self._msg_type
+
+    """ Getter for payload """
+    def get_payload(self):
+        return self._payload
