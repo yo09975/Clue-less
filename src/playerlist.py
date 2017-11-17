9e77979974da78c1ffb4c5cd6b4b74933902b6b5
steve <stephensmeal@gmail.com>
2017-11-16 20:13:29 -0500

    Recommitting Player Assoc Classes To Fix Branch Issue
    Recommitting the following files since the past committ was done
    on an incorrect branch.
    player.py
    playerlist.py
    playerstatus.py

--- src/playerlist.py
+++ src/playerlist.py
@@ -1,42 +1,24 @@
-"""playerlist.py."""
-
-from card import Card
-from player import Player
-from cardtype import CardType
+"""playerlist.py"""
+from src.player import Player
 
 
-class SingletonType(type):
-    def __call__(cls, *args, **kwargs):
-        try:
-            return cls.__instance
-        except AttributeError:
-            cls.__instance = super(
-                SingletonType, cls).__call__(*args, **kwargs)
-            return cls.__instance
-
-
-class PlayerList(object):
+class PlayerList:
     """Represents all Players in the game.
 
     PlayerList class in the Game Management Subsystem. Stores all Player
     objects in a list that is stored globally to allow for tracjing of game
     information such as turn sqeuencing, player location, etc. This class
     is a Singleton.
-
-    Arrtibutes:
-    __instance - Instance of the Singleton class
-    _player_list - List of Player objects representing the Player order
-
     """
 
-    __metaclass__ = SingletonType
+    _player_list = []
+    """List of Player objects representing the Player order"""
 
-    __instance = None
+    class __impl:
+        """Implementation of the Singleton class"""
 
-    def __init__(self):
-        """Constructor"""
-        __instance = SingletonType(self)
-        self._player_list = []
+    __instance = __impl()
+    """The private class attribute holding the "one and only instance"""
 
     def get_instance(self):
         """Returns the instance of PlayerList"""
@@ -44,22 +26,33 @@ class PlayerList(object):
 
     def add_player(self, player: Player):
         """Accepts a Player object to add to the end of the list"""
-        _player_list.append(Player)
+        self._player_list.append(player)
 
-    def get_next_turn(self) -> Player:
+    def get_next_turn(self, current: int) -> Player:
         """Returns the next active Player in the list"""
-        return Player(Card(
-            'get_next_turn Player', CardType.SUSPECT, 'Player123'))
+        if (current < len(self._player_list)) and (current > -1):
+            next_player = (current + 1) % len(self._player_list)
+            return self._player_list[next_player]
+        else:
+            return None
 
     def get_next_player(self, after_player: Player) -> Player:
         """Accepts a Player object and returns Player that is next"""
-        return Player(Card(
-            'get_next_player Player', CardType.SUSPECT, 'Player123'))
+        if after_player in self._player_list:
+            current_index = self._player_list.index(after_player)
+            next_player = (current_index + 1) % len(self._player_list)
+            return self._player_list[next_player]
+        else:
+            return None
 
     def get_players(self) -> list:
         """Returns a List of all Players"""
-        return _player_list
+        return self._player_list
 
     def get_player(self, uuid: str) -> Player:
         """Accepts a UUID and returns the corresponding Player object"""
-        return Player(Card(uuid, CardType.SUSPECT, 'Player123'))
+        for player in self._player_list:
+            if uuid == player.get_token():
+                player_index = self._player_list.index(player)
+                return self._player_list[player_index]
+        return None
