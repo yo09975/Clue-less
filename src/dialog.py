3cbe777e54e27ad007d84d36d1738039e6ea4fd0
Michael Myer <mrmyer80@gmail.com>
2017-11-24 21:52:16 -0500

    moving ui subsystem src files into src directory.

---
+++ src/dialog.py
@@ -0,0 +1,18 @@
+"""dialog.py"""
+
+
+class Dialog(object):
+    """
+    Pop-up that contains a message and has two buttons.  Initiated with references
+    to the actions that should be taken for each button.
+    """
+
+    message = None  # a string representing the message to display
+    left_button = None  # a button representing a command
+    right_button = None # a button representing a command
+
+    def __init__(self, message, left_button, right_button):
+        """Constructor for Dialog which accepts two Button objects and a String"""
+        self.__message = message
+        self.__left_button = left_button
+        self.__right_button = right_button
