4361d57642748a880daadf45b05b54f63b36fb4e
Sean <yo09975@umbc.edu>
2017-11-30 18:55:23 -0500

    Client: Add connect/disconnect logic

--- src/network/clientnetworkinterface.py
+++ src/network/clientnetworkinterface.py
@@ -12,11 +12,30 @@ class ClientNetworkInterface(metaclass=Singleton):
         self.client_socket = None
 
     """ Connect to the ServerNetworkInterface """
-    def connect(self, ip, port):
-        raise NotImplementedError
+    def connect(self, ip):
+        if self.is_connected():
+            print('Already connected to server!')
+            return True
+
+        # Create a TCP socket connection to server
+        try:
+            self.client_socket = create_connection((ip, ServerNetworkInterface.PORT), 5)
+        except Exception as e:
+            print(f'Error: Failed to connect to ({ip},{ServerNetworkInterface.PORT}). Exception is {e}.')
+            return False
+    
+    """ Returns whether we have a valid connection to the server """
+    def is_connected(self):
+        return self.client_socket != None
+
     """ Disconnect from the ServerNetworkInterface """
     def disconnect(self):
-        raise NotImplementedError
+        if self.is_connected():
+            # Close the connection
+            self.client_socket.close()
+            # Zeroize our connection variable
+            self.client_socket = None
+
     """ Send message to a GameSocket """
     def send_message(self, uuid, message):
         raise NotImplementedError
@@ -26,3 +45,14 @@ class ClientNetworkInterface(metaclass=Singleton):
     """ Send message to all GameSocket """
     def send_all(self, message):
         raise NotImplementedError
+
+if __name__ == '__main__':
+    try:
+        c = ClientNetworkInterface()
+        c.connect('localhost')
+        c2 = ClientNetworkInterface()
+        c2.connect('localhost')
+    except KeyboardInterrupt:
+        print('Interrupted')
+        exit(0)
+
