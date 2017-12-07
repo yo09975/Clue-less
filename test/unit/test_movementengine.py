10b6f406366d5ef7dfba561c24dbcfed16c067f0
Sean <yo09975@umbc.edu>
2017-12-07 01:54:33 -0500

    MovementEngine: Fixed unit tests
    Previously, the ME unit tests manually added players to the PlayerList.
    This caused all sorts of problems. Instead, hardcoded in some of the
    default card data and use GameState to initialize PlayerList with suspects
    from the data file. If there is time, this should be cleaned up further,
    but for now it' working.

--- test/unit/test_movementengine.py
+++ test/unit/test_movementengine.py
@@ -15,17 +15,16 @@ from src.move import Move
 from src.card import Card
 from src.cardtype import CardType
 from src.playerstatus import PlayerStatus
-
-MIS_SCA_CARD = Card('Test Card 1 Name', CardType.SUSPECT, 'TEST111ID')
-COL_MUS_CARD = Card('Test Card 2 Name', CardType.SUSPECT, 'TEST222ID')
-MIS_SCA = Player(MIS_SCA_CARD)
-MIS_SCA.set_status(PlayerStatus.ACTIVE)
-COL_MUS = Player(COL_MUS_CARD)
-COL_MUS.set_status(PlayerStatus.ACTIVE)
+from src.gamestate import GameState
+# Create two cards from the data in the data file
 player_list = PlayerList()
-player_list.clear()
-player_list.add_player(MIS_SCA)
-player_list.add_player(COL_MUS)
+gs = GameState()
+scarlet_card = Card('Miss Scarlet', CardType.SUSPECT, 'Miss Scarlet')
+mustard_card = Card('Colonel Mustard', CardType.SUSPECT, 'Colonel Mustard')
+scarlet_player = player_list.get_player(scarlet_card.get_id())
+scarlet_player.set_status(PlayerStatus.ACTIVE)
+mustard_player = player_list.get_player(mustard_card.get_id())
+mustard_player.set_status(PlayerStatus.ACTIVE)
 
 me = MovementEngine()
 
@@ -37,13 +36,13 @@ _6x2 = me._board.get_location("6x2")
 
 def test_serialize_move():
     """ Test that serializing a Move object gives the correct string """
-    move = Move(MIS_SCA.get_card_id(), _4x0._key)
+    move = Move(scarlet_player.get_card_id(), _4x0._key)
     move_serial = move.serialize()
-    assert move_serial.find(f'"character": "TEST111ID", "destination": "{_4x0._key}"') != -1
+    assert move_serial.find(f'"character": "Miss Scarlet", "destination": "{_4x0._key}"') != -1
 
 def test_deserialize_move():
     """ Test that deserializing a Move string gives the correct object """
-    move = Move(MIS_SCA.get_card_id(), _4x0._key)
+    move = Move(scarlet_player.get_card_id(), _4x0._key)
     move_serial = move.serialize()
     move_deserialized = Move.deserialize(move_serial)
     assert move_deserialized.get_character_id() == move.get_character_id()
@@ -51,14 +50,14 @@ def test_deserialize_move():
 
 def test_detect_illegal_move():
     """Ask the Movement Engine to check move to non-neighbor."""
-    MIS_SCA.set_location(_4x0._key)
-    COL_MUS.set_location(_6x2._key)
+    scarlet_player.set_location(_4x0._key)
+    mustard_player.set_location(_6x2._key)
 
     # Sanity check - characters are in locations
     assert me._board.get_location("4x0").get_occupant_count() == 1
     assert me._board.get_location("6x2").get_occupant_count() == 1
 
-    move = Move(MIS_SCA.get_card_id(), _5x1._key)
+    move = Move(scarlet_player.get_card_id(), _5x1._key)
 
     assert not me.is_valid_move(move)
 
@@ -66,7 +65,7 @@ def test_detect_illegal_move():
 def test_detect_move_to_occupied_passage():
     """Ask the Movement Engine to check a move to an occupied passage."""
     # Move character to passage blocking Miss Scarlet
-    move = Move(COL_MUS.get_card_id(), _4x1._key)
+    move = Move(mustard_player.get_card_id(), _4x1._key)
     assert me.do_move(move)
 
     # Sanity check - characters are in locations
@@ -74,14 +73,14 @@ def test_detect_move_to_occupied_passage():
     assert me._board.get_location("4x0").get_occupant_count() == 1
 
     # Check validity of moving Miss Scarlett to newly occupied passage
-    move2 = Move(MIS_SCA.get_card_id(), _4x1._key)
+    move2 = Move(scarlet_player.get_card_id(), _4x1._key)
     assert not me.is_valid_move(move2)
 
 
 def test_detect_move_to_unoccupied_passage():
     """Ask the Movement Engine to check a valid move."""
     # Move character out of Miss Scarlet's way
-    move = Move(COL_MUS.get_card_id(), _5x1._key)
+    move = Move(mustard_player.get_card_id(), _5x1._key)
     assert me.do_move(move)
 
     # Sanity check - characters are in locations
@@ -89,14 +88,14 @@ def test_detect_move_to_unoccupied_passage():
     assert me._board.get_location("4x0").get_occupant_count() == 1
 
     # Check validity of moving Miss Scarlett to newly unoccupied passage
-    move2 = Move(MIS_SCA.get_card_id(), _4x1._key)
+    move2 = Move(scarlet_player.get_card_id(), _4x1._key)
     assert me.is_valid_move(move2)
 
 
 def test_detect_move_to_occupied_room():
     """Ask the Movement Engine to check validity of move to occupied room."""
     # Move Miss Scarlet out of her starting point
-    move = Move(MIS_SCA.get_card_id(), _4x1._key)
+    move = Move(scarlet_player.get_card_id(), _4x1._key)
     assert me.do_move(move)
 
     # Sanity check - characters are in locations
@@ -104,14 +103,14 @@ def test_detect_move_to_occupied_room():
     assert me._board.get_location("5x1").get_occupant_count() == 1
 
     # Check validity of moving Miss Scarlet to Lounge
-    move2 = Move(MIS_SCA.get_card_id(), _5x1._key)
+    move2 = Move(scarlet_player.get_card_id(), _5x1._key)
     assert me.is_valid_move(move2)
 
 
 def test_detect_move_to_unoccupied_room():
     """Ask the Movemetn Engine to check validity of move to empty room."""
     # Move player out of Lounge
-    move = Move(COL_MUS.get_card_id(), _3x1._key)
+    move = Move(mustard_player.get_card_id(), _3x1._key)
     assert me.do_move(move)
 
     # Sanity check - characters are in locations
@@ -119,7 +118,7 @@ def test_detect_move_to_unoccupied_room():
     assert me._board.get_location("3x1").get_occupant_count() == 1
 
     # Check validity of moving Miss Scarlet to Lounge
-    move2 = Move(MIS_SCA.get_card_id(), _5x1._key)
+    move2 = Move(scarlet_player.get_card_id(), _5x1._key)
     assert me.is_valid_move(move2)
     assert me.do_move(move2)
 
@@ -127,7 +126,7 @@ def test_detect_move_to_unoccupied_room():
 def test_execute_non_neighbor_move():
     """Ensure MovementEngine.do_move() works when someone is suggested."""
     hall_occ = me._board.get_location("3x1").get_occupant_count()
-    move = Move(MIS_SCA.get_card_id(), _3x1._key)
+    move = Move(scarlet_player.get_card_id(), _3x1._key)
 
     assert not me.is_valid_move(move)
 
