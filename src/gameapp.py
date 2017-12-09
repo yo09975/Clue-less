8fe9e5c7a23073c561388b0d840eea7240d3731c
Patrick May <patricknmay@gmail.com>
2017-12-08 21:45:12 -0500

    Add message center for NOTIFY message types.

--- src/gameapp.py
+++ src/gameapp.py
@@ -34,6 +34,7 @@ class GameApp:
         self._gameDisplay = pygame.display.set_mode((1410, 900))
         self._crashed = False
 
+        self._message_log = []
 
         self._background = pygame.image.load('resources/gameplaygui.jpg')
 
@@ -167,6 +168,12 @@ class GameApp:
             message = cni.get_message()
             if message is not None:
                 print('\n', message.get_msg_type(), message.get_payload())
+                if message.get_msg_type() == MessageType.NOTIFY
+                    font = pygame.font.SysFont('Comic Sans MS', 16)
+                    text_message = font.render(message.get_payload(), False, (0, 0, 0))
+                    self._message_log.append(text_message)
+                    # Keep last 6 messages
+                    self._message_log = self._message_log[-5:]
             else:
                 print('.', end='', flush=True)
 
@@ -180,6 +187,10 @@ class GameApp:
 
             # Always draw game pieces on the game board
             # TODO render game pieces
+
+            # Always render message log
+            for i, text_message in enumerate(self._message_log):
+                self._gameDisplay.blit(text_message, (20, 775 + 20 * i))
 
             # Display views based on state
             if self._state == PlayerState.SELECT_PLAYER:
