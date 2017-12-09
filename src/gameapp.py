e4813be513d7952c52b0b0348c48791762c7f16f
Michael Myer <mrmyer80@gmail.com>
2017-12-09 10:42:44 -0500

    disable start button until minimum players are ready.

--- src/gameapp.py
+++ src/gameapp.py
@@ -206,6 +206,14 @@ class GameApp:
 
                         self._char_picker_dialog.set_unavailable_players(unavailable)
 
+                        for player in player_list:
+                            player_count = 0
+
+                            if player['uuid']:
+                                player_count += 1
+
+                            self._char_picker_dialog.get_bottom_button().set_enabled(player_count > 2)
+
                     elif message.get_msg_type() == MessageType.PLAYER_HAND:
                         # Start game
                         self._state = PlayerState.WAIT_FOR_TURN
