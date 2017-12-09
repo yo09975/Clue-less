5fdc0b2bb3b19f01ee68dc754f416e3b6a4fd333
Sean <yo09975@umbc.edu>
2017-12-09 15:55:30 -0500

    SuggestionEngine: Fix calls to SNI functions

--- src/suggestion_engine.py
+++ src/suggestion_engine.py
@@ -8,7 +8,7 @@ from src.playerlist import PlayerList
 from src.player import Player
 from src.playerstatus import PlayerStatus
 from src.network.message import Message, MessageType
-from src.network.servernetworkinterface import ServerNetworkInterface
+from src.network.servernetworkinterface import ServerNetworkInterface as SNI
 
 
 class SuggestionEngine:
@@ -35,7 +35,7 @@ class SuggestionEngine:
     def make_suggestion(self, suggestion: Suggestion):
         """Makes a suggestion. Also offers to move the suggested character to the suggested room."""
         """These are some acrobatics to initialize the suggesting player and responding player."""
-
+        sni = SNI()
         current = self.game_state.get_current_player()
         players = PlayerList.get_players()
         suggesting_player = players[current]
@@ -44,60 +44,61 @@ class SuggestionEngine:
         """Iterating through each player in PlayerList. If they are able to refute, we send a SUGGESTION_REQUEST to
         them asking that they select a card with which to refute the suggestion. If a player cannot refute we let
         all users know they were unable to refute."""
-        suggestion_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.SUGGESTION_NOTIFY,
+        suggestion_msg = Message(sni.get_uuid(), MessageType.SUGGESTION_NOTIFY,
                                  suggesting_player.get_character().get_id() + suggestion.serialize())
-        ServerNetworkInterface.send_all(suggestion_msg)
+        sni.send_all(suggestion_msg)
 
         while responder != suggesting_player:
             hand = responder.get_hand()
             if hand.contains_card(suggestion.get_room()) or hand.contains_card(suggestion.get_character()) or \
                     hand.contains_card(suggestion.get_weapon()):
-                response_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.SUGGESTION_REQUEST,
+                response_msg = Message(sni.get_uuid(), MessageType.SUGGESTION_REQUEST,
                                        suggestion.serialize())
-                ServerNetworkInterface.send_message(responder.get_uuid(), response_msg)
-                response_notification = Message(ServerNetworkInterface.get_uuid(), MessageType.SUGGESTION_NOTIFY,
+                sni.send_message(responder.get_uuid(), response_msg)
+                response_notification = Message(sni.get_uuid(), MessageType.SUGGESTION_NOTIFY,
                                                 responder.get_character().get_id() + "disproved the suggestion.")
-                ServerNetworkInterface.send_all(response_notification)
+                sni.send_all(response_notification)
                 return True
             else:
-                could_not_respond_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.SUGGESTION_NOTIFY,
+                could_not_respond_msg = Message(sni.get_uuid(), MessageType.SUGGESTION_NOTIFY,
                                                 responder.get_character().get_id() + " could not disprove.")
-                ServerNetworkInterface.send_all(could_not_respond_msg)
+                sni.send_all(could_not_respond_msg)
                 responder = PlayerList.get_next_player(responder)
 
-        no_response_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.SUGGESTION_NOTIFY,
+        no_response_msg = Message(sni.get_uuid(), MessageType.SUGGESTION_NOTIFY,
                                   "The suggestion could not be refuted.")
-        ServerNetworkInterface.send_all(no_response_msg)
+        sni.send_all(no_response_msg)
         return False
 
     def answer_suggestion(self, response: Card):
         """Allows a player to answer a suggestion."""
-
+        sni = SNI()
         current = self.game_state.get_current_player()
         players = PlayerList.get_players()
         suggesting_player = players[current]
-        response_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.SUGGESTION_NOTIFY, response.serialize())
-        ServerNetworkInterface.send_message(suggesting_player.get_uuid(), response_msg)
+        response_msg = Message(sni.get_uuid(), MessageType.SUGGESTION_NOTIFY, response.serialize())
+        sni.send_message(suggesting_player.get_uuid(), response_msg)
 
     def make_accusation(self, suggestion: Suggestion, accuser: Player):
         """Makes an accusation; if correct the game is won, if not the player loses and everyone is notified."""
 
+        sni = SNI()
         if suggestion == self.game_state.get_solution():
-            correct_accusation_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.ACCUSATION_NOTIFY,
+            correct_accusation_msg = Message(sni.get_uuid(), MessageType.ACCUSATION_NOTIFY,
                                      suggestion.serialize())
-            ServerNetworkInterface.send_all(correct_accusation_msg)
-            correct_notification_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.NOTIFY,
+            sni.send_all(correct_accusation_msg)
+            correct_notification_msg = Message(sni.get_uuid(), MessageType.NOTIFY,
                                        accuser.get_character().get_id() + " made a correct accusation.")
-            ServerNetworkInterface.send_all(correct_notification_msg)
+            sni.send_all(correct_notification_msg)
             return True
         else:
             accuser.set_status(PlayerStatus.LOST)
-            accusation_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.ACCUSATION_NOTIFY,
+            accusation_msg = Message(sni.get_uuid(), MessageType.ACCUSATION_NOTIFY,
                                      suggestion.serialize())
-            ServerNetworkInterface.send_all(accusation_msg)
-            notification_msg = Message(ServerNetworkInterface.get_uuid(), MessageType.NOTIFY,
+            sni.send_all(accusation_msg)
+            notification_msg = Message(sni.get_uuid(), MessageType.NOTIFY,
                                        accuser.get_character().get_id() + " made an incorrect accusation.")
-            ServerNetworkInterface.send_all(notification_msg)
+            sni.send_all(notification_msg)
             return False
 
     def is_valid_suggestion(self, suggesting_player: Player, suggestion: Suggestion) -> bool:
