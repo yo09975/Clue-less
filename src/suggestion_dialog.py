ea790a440a05dd81c83c5283d23c441b30ab37ed
Patrick May <patricknmay@gmail.com>
2017-12-10 14:57:55 -0500

    Fixed dumbass mistake.

--- src/suggestion_dialog.py
+++ src/suggestion_dialog.py
@@ -82,7 +82,7 @@ class SuggestionDialog(Dialog):
             return None
 
     def set_room_id(self, room_card_id):
-        for self._room_picker.enable_all()
+        self._room_picker.enable_all()
         for i, c in enumerate(self._rooms):
             if c.get_id() != room_card_id:
                 self._room_picker.disable_button(i)
